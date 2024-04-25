2fa9db78ba9a307f37b60f7378ff005de7e3d75c
Onur Baykal <theaob@gmail.com>
2024-04-25 14:37:53 +0300

    implement score persistance

--- singletons/score_manager.gd
+++ singletons/score_manager.gd
@@ -1,13 +1,14 @@
 extends Node
 
 const DEFAULT_SCORE: int = 1000
+const SCORES_PATH = "user://animals.json"
 
 var _level_scores: Dictionary = {}
 
 var _selected_level: int = 1
 
 func _ready():
-	pass
+	load_from_disk()
 
 func set_selected_level(level: int) -> void:
 	_selected_level = level
@@ -22,7 +23,24 @@ func ensure_key_existance(level: String):
 func set_score_for_level(score: int, level: String):
 	if _level_scores[level] > score:
 		_level_scores[level] = score
+		save_to_disk()
 
 func get_best_for_level(level: String) -> int:
 	ensure_key_existance(level)
 	return _level_scores[level]
+	
+func save_to_disk():
+	var file = FileAccess.open(SCORES_PATH, FileAccess.WRITE)
+	var score_json = JSON.stringify(_level_scores)
+	file.store_string(score_json)
+	
+func load_from_disk():
+	var file = FileAccess.open(SCORES_PATH, FileAccess.READ)
+	
+	if file == null:
+		# file does not exist
+		save_to_disk()
+		return
+	
+	var data = file.get_as_text()
+	_level_scores = JSON.parse_string(data)
